4267569772220b767f371294a8759af742f5647f
Luke Petschauer <luke.petschauer@gmail.com>
2015-12-26 13:10:09 -0800

    add the PublishedManager custom model manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', "Draft"),
@@ -18,10 +23,11 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
-

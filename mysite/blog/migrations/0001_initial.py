3d3042cdb5991164087b1a640ce42681f5bdcc41
Luke Petschauer <luke.petschauer@gmail.com>
2015-12-26 12:29:40 -0800

    first commit

---
+++ mysite/blog/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.utils.timezone import utc
+import datetime
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.CharField(max_length=250)),
+                ('slug', models.SlugField(max_length=250, unique_for_date='publish')),
+                ('body', models.TextField()),
+                ('publish', models.DateTimeField(default=datetime.datetime(2015, 12, 26, 20, 19, 5, 845257, tzinfo=utc))),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('status', models.CharField(max_length=10, default='draft', choices=[('draft', 'Draft'), ('published', 'Published')])),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='blog_posts')),
+            ],
+            options={
+                'ordering': ('-publish',),
+            },
+        ),
+    ]

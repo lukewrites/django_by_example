0c22adcd8591709d38a0c78fc0946291e073784f
Luke Petschauer <luke.petschauer@gmail.com>
2015-12-26 17:18:03 -0800

    get rid of settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -101,3 +101,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'microflaskinpoetry@gmail.com'
+EMAIL_HOST_PASSWORD = 'modernartisbullshitandi\'maphilistine'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True

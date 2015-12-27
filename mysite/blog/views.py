8e88b79880363e8ec9cab9e5421fcf127cc1ab8f
Luke Petschauer <luke.petschauer@gmail.com>
2015-12-26 19:48:29 -0800

    working comments feature

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,28 +4,36 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)  # 3 posts on each page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:  # if the page isn't an integer, show 1st page.
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#         # if the page is out of range, show the last page.
-#     return render(request,
-#                   'blog/post/list.html',
-#                   {'page': page,
-#                   'posts': posts})
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)  # 3 posts on each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:  # if the page isn't an integer, show 1st page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        # if the page is out of range, show the last page.
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                  'posts': posts,
+                  'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,

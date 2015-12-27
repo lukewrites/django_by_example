57081c7f3d746736c5df76bbf89164d3ddeb08e2
Luke Petschauer <luke.petschauer@gmail.com>
2015-12-26 16:11:16 -0800

    add the paginator view and template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts on each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:  # if the page isn't an integer, show 1st page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        # if the page is out of range, show the last page.
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                  'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,

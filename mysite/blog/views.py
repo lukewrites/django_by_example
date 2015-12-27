c8eed4e0ce11628f697f5079fa52e1c7e172aa89
Luke Petschauer <luke.petschauer@gmail.com>
2015-12-26 17:41:49 -0800

    update gitignore

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -34,3 +36,20 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+def post_share(request, post_id):
+    # retrieve post by ID
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'post': post,
+                  'form': form}
+                  )

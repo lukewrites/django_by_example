c2304b58369c62b487709d1833ae20df993a3dfe
Luke Petschauer <luke.petschauer@gmail.com>
2015-12-29 23:00:36 -0800

    similar posts working

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
@@ -56,11 +57,22 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    # values_list is a queryset that usually returns tuples;
+    # the (flat=True) makes it a regular happy [] list.
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                         .exclude(id=post.id)
+                    # so it won't include THIS post.
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                          .order_by('-same_tags', '-publish')[:4]
+                    #TODO need to look up .annotate().
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                   'comments': comments,
-                  'comment_form': comment_form}
+                  'comment_form': comment_form,
+                  'similar_posts': similar_posts}
                   )
 
 def post_share(request, post_id):
